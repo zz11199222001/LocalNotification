97c7f92c03895256368d2ce53ba56107f40bd171
zhuhe <congratulatezh@163.com>
2017-08-31 11:17:00 +0800

    test

--- EXE/EXE/ViewController.m
+++ EXE/EXE/ViewController.m
@@ -9,8 +9,8 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
-    
     [ZQLocalNotification NotificationType:CountdownNotification Identifier:@"112122" activityId:1900000 alertBody:@"开始测试了" alertTitle:@"题头" alertString:@"测试" withTimeDay:1 hour:0 minute:0 second:0];
 
 }
